6c76960039fad2475ec80e1957703b34129313aa
Ashraful Islam <ashraful171790@gmail.com>
2026-09-24 15:22:19 +0600

    book vibe project initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/shared/navBar/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +23,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme= 'light'
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        
+          <NavBar></NavBar>
+
+
+
+          {children}
+          
+          </body>
     </html>
   );
 }
